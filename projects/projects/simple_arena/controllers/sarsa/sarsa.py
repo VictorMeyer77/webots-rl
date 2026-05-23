c5b34c01a2a54394f2214f1b695fca483a152331
VictorMeyer77 <=>
2026-05-23 15:49:57 +0200

    sarsa recover

--- projects/projects/simple_arena/controllers/sarsa/sarsa.py
+++ projects/projects/simple_arena/controllers/sarsa/sarsa.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         TrainerAgent(epuck, config).run(MAX_TIMESTEP)
     else:
         model = ModelValueTable(
-            model_dir="/Users/victormeyer/Dev/Self/webots-rl/projects/.model/sarsa"
+            model_dir="/Users/victormeyer/Dev/Self/webots-rl/projects/.model/simple_arena/sarsa"
         )
 
         epuck = EpuckSarsaController(
